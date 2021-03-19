a9bb0507a7addef116421d39cc72c124018ca42a
Code Ninjas 4 <codeninjassouthjordan@gmail.com>
2021-03-19 12:23:54 -0600

    xdfcv

---
+++ Anabel-DroppingBombsPart4/Assets/Scripts/ExplosionClear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplosionClear : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //private ParticleSystem particleSmoke;
+    void Start()
+    {
+        //particleSmoke = gameObject.GetComponentInChildren<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if(!particleSmoke.IsAlive())
+        {
+            Destroy(gameObject);
+        }*/
+    }
+}
---
+++ Anabel-DroppingBombsPart4/Assets/Scripts/TriggerExplosion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerExplosion : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //[Header("Explosion Parts)]
+    //public GameObject explosion
+    void Start()
+    {
+        //Instantiate(explosion, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
